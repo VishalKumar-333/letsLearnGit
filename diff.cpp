87b4de331f6ca9f6ee9b8d43535a9bc045c5ed5a
Vishal kumar <vishal.kumar.vs333@gmail.com>
2022-10-07 10:28:50 +0530

    adding diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,16 @@
+ #include<iostream>
+ using namespace std;
+ class Geeks
+{
+    // Access specifier
+    public:
+  
+    // Data Members
+    string geekname;
+  
+    // Member Functions()
+    void printname()
+    {
+       cout << "Geekname is: " << geekname;
+    }
+};
